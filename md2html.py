28dfcfec6d98f70a36d2c6602e67946dd63c4a0a
neov2 <neov2@illinois.edu>
2022-02-03 23:39:26 -0800

    removed blogs and credited supercyp

--- md2html.py
+++ md2html.py
@@ -9,7 +9,6 @@ from os.path import isfile, join, isfile, islink, isdir
 # globals
 path_to_index = './src/index'
 path_to_projects = './src/projects'
-path_to_blogs = './src/blogs'
 path_to_templates = './templates'
 path_to_build = './build'
 path_to_assets = './assets'
@@ -39,31 +38,6 @@ def generate_projects_html():
 
     return projects_html
 
-# parses blogs directory and returns blog html
-def generate_blogs_html():
-    # get blogs
-    blogs = [f for f in listdir(path_to_blogs) if isfile(join(path_to_blogs, f))] 
-    blogs = list(filter(lambda x: x[x.rindex('.')+1:] == "json", blogs))
-
-    # read each project's json and add to projects_html
-    blogs_html = []
-    for b in blogs:
-        with open(join(path_to_blogs, b), 'r') as b_json:
-            b_json_dict = json.loads(b_json.read())
-            b_no_ext = b[:b.rindex('.')]
-            b_html = \
-                "<h2> " + b_json_dict["title"] + " </h2>\n" + \
-                "<h5> written " + b_json_dict["date"] + " </h5>\n" + \
-                "<h4> " + b_json_dict["blog_summary"] + " </h4>\n" + \
-                "<a href=\"" + b_no_ext + ".html\">read more</a>" 
-            blogs_html.append(b_html)
-
-    # surround each project with div
-    blogs_html = list(map(lambda x: "<div class=\"blog_entry\">\n" + x + "\n</div>", blogs_html))
-    blogs_html.insert(0, "<p> a little blog </p> ")
-
-    return blogs_html
-
 # combine index template and blog/projects html to form index.html
 def generate_index_html():
     # STEP 1: parse index.json and replace {{}} occurrences
@@ -80,42 +54,9 @@ def generate_index_html():
     # STEP 2: replace [[content]]
     projects_html = generate_projects_html()
     projects_html_reduce = functools.reduce(lambda a,b: a + '\n' + b, projects_html) 
-    blogs_html = generate_blogs_html()
-    blogs_html_reduce = functools.reduce(lambda a,b: a + '\n' + b, blogs_html) 
-    index_txt = index_txt.replace("[[content]]", projects_html_reduce + blogs_html_reduce)
+    index_txt = index_txt.replace("[[content]]", projects_html_reduce)
 
     return index_txt
-
-## Functions for converting blog markdown files and generating html ##
-def generate_blogs():
-    # get blogs
-    blogs = [f for f in listdir(path_to_blogs) if isfile(join(path_to_blogs, f))] 
-    blogs = list(filter(lambda x: x[x.rindex('.')+1:] == "json", blogs))
-    blogs = list(map(lambda x: x[:x.rindex('.')], blogs))
-
-    # generate html for each blog site
-    for b in blogs: 
-        # [[content]] html
-        content_html = pypandoc.convert_file(join(path_to_blogs, b + '.md'), 'html', format='md')
-
-        # json dict
-        b_json_dict = {}
-        with open(join(path_to_blogs, b + '.json'), 'r') as b_json:
-            b_json_dict = json.loads(b_json.read())
-        
-        # parse {{}} and replace
-        b_txt = ""
-        with open(join(path_to_templates, 'article.html'), 'r') as article_html:
-            b_txt = article_html.read()
-            for k in b_json_dict.keys():
-                b_txt = b_txt.replace("{{" + k + "}}", b_json_dict[k])
-
-        # replace content
-        b_txt = b_txt.replace("[[content]]", content_html)
-
-        # write html file to build directory
-        with open(join(path_to_build, b + '.html'), 'w') as f:
-            f.write(b_txt)
 
 ## Util functions for creating and deleting files ##
 # clear build directory
@@ -145,9 +86,6 @@ def create_build():
 
     # copy assets
     shutil.copytree(path_to_assets, join(path_to_build, 'assets'))
-
-    # generate blog pages
-    generate_blogs()
 
 if __name__ == '__main__':
     clear_build()
